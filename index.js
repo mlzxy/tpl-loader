2274100fb292bc4eafbdb42a0e291d8a7bdac9d3
Xinyu Zhang <xiz368@eng.ucsd.edu>
2021-04-24 15:22:09 +0800

    patch to work with latest webpack

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ var _ = require('lodash');
 
 module.exports = function (source) {
   this.cacheable && this.cacheable();
-  var template = _.template(source, null, this.options.tplSettings);
+  var template = _.template(source, null, null);
   return 'var _ = require(\'lodash\');\nmodule.exports = ' + template;
 };
